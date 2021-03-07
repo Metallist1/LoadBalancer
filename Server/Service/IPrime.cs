16723faf06bedb7935c00bcb2de70df43b3886d8
Nedas Surkus <nedassurkus@gmail.com>
2021-03-07 11:29:39 +0100

    Created server

---
+++ Server/Service/IPrime.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service
+{
+   public interface IPrime
+    {
+        Boolean isPrime(string prime);
+        List<string> countPrime(string from, string to);
+    }
+}
---
+++ Server/Service/Prime.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service
+{
+    public class Prime : IPrime
+    {
+        public List<string> countPrime(string from, string to)
+        {
+            List<string> myList = new List<string>() {
+                "one",
+                "two",
+                "three",
+                };
+            return myList;
+        }
+
+        public bool isPrime(string prime)
+        {
+            return false;
+        }
+    }
+}
