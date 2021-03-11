52bc2b4974cdb7e2d61a1418a1594f3ac1566e24
Nedas Surkus <nedassurkus@gmail.com>
2021-03-11 22:52:32 +0100

    First fully working version

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,9 +1,10 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+var https = require('https');
 
   let previousServer = 0;
-  let arrayOfServers = ['https://loadbalancerino.azurewebsites.net','http://loadbalancer2.azurewebsites.net/'];
+  let arrayOfServers = ['loadbalancerino.azurewebsites.net','loadbalancer2.azurewebsites.net'];
 io.on("connection", socket => {
 
   function selectServer() {
@@ -11,16 +12,72 @@ io.on("connection", socket => {
 		return arrayOfServers[previousServer++];
 	} 
   
+    function createOption(data, type, path){
+		if(type === 'GET'){
+			return options = {
+			  hostname: selectServer(),
+			  port: 443,
+			  path: path,
+			  method: type,
+				  headers: {
+					'Content-Type': 'application/json'
+				  }
+			}
+		}
+		
+		return options = {
+		  hostname: selectServer(),
+		  port: 443,
+		  path: path,
+		  method: type,
+		  headers: {
+			'Content-Type': 'application/json',
+			'Content-Length': data.length
+		  }
+		}
+	} 
    socket.on("isPrime", docId => {
-	   console.log(selectServer());
-	   socket.to(doc.id).emit("document", doc);
-    socket.emit("isPrime", 'response');
-  });
-  
+	const option = createOption ('' , 'GET', '/api/primes/' + docId);
+			console.log(option);
+			  const req = https.request(option, res => {
+			  console.log(`statusCode: ${res.statusCode}`)
+
+			  res.on('data', d => {
+				console.log(JSON.parse(d).response);
+				socket.emit("isPrime", JSON.parse(d).response);
+			  })
+			})
+
+			req.on('error', error => {
+			  console.error(error)
+			})
+
+			//req.write(data)
+			req.end()
+    });
   socket.on("countPrime", doc => {
-	  
-	   console.log(selectServer());
-    socket.emit("countPrime", 'response');
+	  const data = JSON.stringify({
+			from: Number(doc.before),
+			to: Number(doc.after)
+		});
+  console.log(data);
+  	const option = createOption ( data, 'POST', '/api/primes/');
+
+			  const req = https.request(option, res => {
+			  console.log(`statusCode: ${res.statusCode}`)
+
+			  res.on('data', d => {
+				console.log(JSON.parse(d).response);
+				socket.emit("countPrime", JSON.parse(d).response);
+			  })
+			})
+
+			req.on('error', error => {
+			  console.error(error)
+			})
+
+			req.write(data)
+			req.end()
   });
   
 
